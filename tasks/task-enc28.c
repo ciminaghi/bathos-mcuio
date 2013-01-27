3c784aa3101c5d28a8bf4ec2a93681f3e1d8ca7e
Alessandro Rubini <rubini@gnudd.com>
2013-01-27 23:00:24 +0100

    task-enc28: make it compile under bathos
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- tasks/task-enc28.c
+++ tasks/task-enc28.c
@@ -1,9 +1,7 @@
-#include <ssw/task.h>
-#include <ssw/init.h>
-#include <ssw/module.h>
-#include <ssw/gpio.h>
-#include <ssw/spi.h>
-#include <ssw/enc28j60.h>
+#include <bathos/bathos.h>
+#include <bathos/enc28j60.h>
+#include <arch/spi.h>
+#include <arch/gpio.h>
 
 /* SPI configuration */
 #define GPIOCS 10
@@ -19,58 +17,56 @@ const struct enc28_cfg enc28_cfg = {
 	.spi_cfg = &spi_cfg,
 	/* This is an IP address in my network */
 	.ipaddr = {192, 168,16, 201},
-	/* And this is in the private range, with today's date */
-	.macaddr = {0x02, 0x27, 0x07, 0x20, 0x10, 0x0},
+	/* And this is in the private range */
+	.macaddr = {0x02, 0x33, 0x44, 0x55, 0x66, 0x77},
 };
+
+static struct enc28_dev enc28_dev = {
+	.cfg = &enc28_cfg,
+};
+
+static int enc28_task_init(void *unused)
+{
+	/* My board has another device on SPI: disable its CS */
+	gpio_set(9, 1);
+	gpio_dir_af(9, 1, 1, 0);
+	gpio_set(9, 1);
+
+	enc28_create(&enc28_dev);
+	return 0;
+}
+
 
 /* the packet we send out is a simple count of runs */
 static unsigned char packet[] = {
 	/* hardwired eth header */
 	[0] = 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,	/* dest: broadcast */
-	[6] = 0x02, 0x27, 0x07, 0x20, 0x10, 0x0,	/* source: us */
-	[12] = 0x12, /* was 8 */ 0x00,			/* proto: IPV4 */
-	/* hardwired IP header */
+	[6] = 0x02, 0x33, 0x44, 0x55, 0x66, 0x77,	/* source: us */
+	[12] = 0x12, /* was 8 */ 0x00,			/* proto: crap */
 
-	/* hardwired UDP header */
-
-	/* data placeholder */
 	[14] = 0x41, 0x42, 0x43, 0x44, 0x45, 0x46	/* temporary hack */
 };
 
 /* The task sends out one packet and reads pending ones */
-static void *task_enc28test(void *arg)
+static void *enc28_task_run(void *arg)
 {
 	struct enc28_dev *dev = arg;
 	static char inpacket[500];
 	int i;
 
-#if 0
+#if 1
 	/* FIXME: temporarily just send out this packet */
 	i = enc28_send(dev, packet, sizeof(packet));
-	printk("send: %i\n", i);
+	printf("send: %i\n", i);
 #else
 	i = enc28_recv(dev, inpacket, sizeof(packet));
-	printk("recv: %i\n", i);
+	printf("recv: %i\n", i);
 #endif
 	return dev;
 }
 
-int __init init_app_enc28(void)
-{
-	struct enc28_dev *dev;
-
-	/* The board has anothe device on SPI: disable its CS */
-	gpio_set(9, 1);
-	gpio_dir_af(9, SSW_GPIO_OUT, SSW_GPIO_AF_GPIO);
-
-	dev = enc28_create(&enc28_cfg);
-	if (!dev) return -1;
-
-	task_register_p(task_enc28test, dev, 100, 10);
-	return 0;
-}
-
-task_initcall(init_app_enc28);
-
-module_request(spi_21xxx);
-module_request(vsprintf_full);
+static struct bathos_task __task t_enc28 = {
+	.name = "spi_test", .period = HZ,
+	.init = enc28_task_init, .job = enc28_task_run,
+	.release = 15
+};
