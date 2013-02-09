9ebe5f5afea67008f9e007616f3bc1b7afc2c29c
Alessandro Rubini <rubini@gnudd.com>
2013-02-09 18:06:00 +0100

    tasks/task-enc28.c: fixed for our config setup
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- tasks/task-enc28.c
+++ tasks/task-enc28.c
@@ -3,18 +3,26 @@
 #include <arch/spi.h>
 #include <arch/gpio.h>
 
-/* SPI configuration */
+
 #define GPIOCS 10
-const struct spi_cfg spi_cfg = {
+
+/*
+ * SPI configuration: we need to declare both the config and the
+ * device since we have no malloc in this crappy OS
+ */
+static const struct spi_cfg spi_cfg = {
 	.gpio_cs = GPIOCS,
 	.pol = 0,
 	.phase = 0,
 	.devn = 0,
 };
 
+static struct spi_dev spi_dev = {
+	.cfg = &spi_cfg,
+};
+
 /* ENC28 configuration */
-const struct enc28_cfg enc28_cfg = {
-	.spi_cfg = &spi_cfg,
+static const struct enc28_cfg enc28_cfg = {
 	/* This is an IP address in my network */
 	.ipaddr = {192, 168,16, 201},
 	/* And this is in the private range */
@@ -22,15 +30,18 @@ const struct enc28_cfg enc28_cfg = {
 };
 
 static struct enc28_dev enc28_dev = {
+	.spi_dev = &spi_dev,
 	.cfg = &enc28_cfg,
 };
 
 static int enc28_task_init(void *unused)
 {
-	/* My board has another device on SPI: disable its CS */
-	gpio_set(9, 1);
+	/* My board has another device on SPI: disable its CS on gpio 9*/
 	gpio_dir_af(9, 1, 1, 0);
 	gpio_set(9, 1);
+	/* This is our gpio 10 */
+	gpio_dir_af(10, 1, 1, 0);
+	gpio_set(10, 1);
 
 	enc28_create(&enc28_dev);
 	return 0;
@@ -51,22 +62,18 @@ static unsigned char packet[] = {
 static void *enc28_task_run(void *arg)
 {
 	struct enc28_dev *dev = arg;
-	static char inpacket[500];
+	static u8 inpacket[50];
 	int i;
 
-#if 1
-	/* FIXME: temporarily just send out this packet */
 	i = enc28_send(dev, packet, sizeof(packet));
-	printf("send: %i\n", i);
-#else
-	i = enc28_recv(dev, inpacket, sizeof(packet));
-	printf("recv: %i\n", i);
-#endif
+	i = enc28_recv(dev, inpacket, sizeof(inpacket));
+
 	return dev;
 }
 
 static struct bathos_task __task t_enc28 = {
-	.name = "spi_test", .period = HZ,
+	.name = "enc28j60", .period = HZ,
+	.arg = &enc28_dev,
 	.init = enc28_task_init, .job = enc28_task_run,
 	.release = 15
 };
