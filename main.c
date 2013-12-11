7e3b3b760c6ee501384e711cff1b3d3b33123014
Davide Ciminaghi <ciminaghi@gnudd.com>
2013-12-11 22:58:08 +0100

    use new events based periodic scheduler

--- main.c
+++ main.c
@@ -4,34 +4,14 @@
  */
 #include <bathos/bathos.h>
 #include <bathos/jiffies.h>
+#include <bathos/event.h>
 #include <arch/hw.h>
 
 int bathos_main(void)
 {
-	struct bathos_task *p;
-	unsigned long now;
-
 	printf("Hello, Bathos is speaking (built on %s)\n", __DATE__);
-
-	for (p = __task_begin; p < __task_end; p++) {
-		printf("Task: %s\n", p->name);
-		if (p->init) p->init(p->arg);
-	}
-
-	now = jiffies;
-	for (p = __task_begin; p < __task_end; p++)
-		p->release += now + 2;
-
-	while (1) {
-		struct bathos_task *t;
-
-		for (t = p = __task_begin; p < __task_end; p++)
-			if (p->release < t->release)
-				t = p;
-		while (time_before(jiffies, t->release))
-			;
-		t->arg = t->job(t->arg);
-		t->release += t->period;
-	}
+	events_init();
+	while(1)
+		handle_events();
 	return 0;
 }
