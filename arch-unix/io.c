8b27c87ef0323169575a535a558657d2ea4b4a1f
Davide Ciminaghi <ciminaghi@gnudd.com>
2013-12-18 13:51:44 +0100

    arch-unix: pre-open bathos_stdout

--- arch-unix/io.c
+++ arch-unix/io.c
@@ -4,11 +4,14 @@
  */
 #include <stdint.h>
 #include <bathos/bathos.h>
+#include <bathos/stdio.h>
 #include <arch/hw.h>
 
 /* We need a main function, called by libc initialization */
 int main(int argc, char **argv)
 {
+	/* Open stdout before getting to main */
+	bathos_stdout = pipe_open("/dev/stdout", BATHOS_MODE_OUTPUT, NULL);
 	bathos_main();
 	return 0;
 }
