b945765f0934567e92c6fe59befeb38b00b3e751
Davide Ciminaghi <ciminaghi@gnudd.com>
2013-12-18 13:33:00 +0100

    lib: add strcmp, strncmp (from linux kernel)

--- include/bathos/string.h
+++ include/bathos/string.h
@@ -2,5 +2,7 @@
 extern char *strcpy(char * d, char *s);
 extern int strlen(char *s);
 extern int strnlen(char *s, int count);
+extern int strcmp(const char *s1, const char *s2);
+extern int strncmp(const char *s1, const char *s2, int n);
 extern void *memcpy(void *d, void *s, int count);
 extern void *memset(void *d, int c, int count);
--- lib/string.c
+++ lib/string.c
@@ -36,6 +36,38 @@ int strnlen(char *s, int count)
 	return sc - s;
 }
 
+
+int strcmp(const char *cs, const char *ct)
+{
+	unsigned char c1, c2;
+
+	while (1) {
+		c1 = *cs++;
+		c2 = *ct++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+	}
+	return 0;
+}
+
+int strncmp(const char *cs, const char *ct, int count)
+{
+	unsigned char c1, c2;
+
+	while (count) {
+		c1 = *cs++;
+		c2 = *ct++;
+		if (c1 != c2)
+			return c1 < c2 ? -1 : 1;
+		if (!c1)
+			break;
+		count--;
+	}
+	return 0;
+}
+
 void *memcpy(void *d, void *s, int count)
 {
 	intptr_t *dl = (intptr_t *)d, *sl = (intptr_t *)s;
