0f78d860a1112c545d65a6d03aa54f728995cb9b
Davide Ciminaghi <ciminaghi@gnudd.com>
2013-12-18 13:49:03 +0100

    add default (weak symbol) implementation for putc

--- lib/stdio.c
+++ lib/stdio.c
@@ -1,5 +1,14 @@
 #include <bathos/stdio.h>
 
+struct bathos_pipe *bathos_stdout;
+
+void __attribute__((weak)) putc(int c)
+{
+	if (!bathos_stdout)
+		return;
+	pipe_write(bathos_stdout, (char *)&c, 1);
+}
+
 int puts(const char *s)
 {
 	while (*s)
