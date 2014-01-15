34c65ca4d0d41d998886e15304ffc91a0cf68d7b
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-01-15 13:39:52 +0100

    FIX periodic_scheduler events based implementation

--- periodic_scheduler.c
+++ periodic_scheduler.c
@@ -2,40 +2,49 @@
 #include <bathos/bathos.h>
 #include <bathos/jiffies.h>
 #include <bathos/event.h>
+#include <bathos/init.h>
 #include <bathos/sys_timer.h>
 #include <arch/hw.h>
 
 declare_event(sys_timer_tick);
 
-static int pts_init(struct event_handler_data *d)
+static struct bathos_task *__find_next_task(void)
+{
+	struct bathos_task *p;
+	struct bathos_task *t;
+	for (t = p = __task_begin; p < __task_end; p++)
+		if (p->release < t->release)
+			t = p;
+	return t;
+}
+
+static int pts_init(void)
 {
 	unsigned long now = jiffies;
-	struct bathos_task *p;
+	struct bathos_task *p, *next;
 	for (p = __task_begin; p < __task_end; p++) {
 		printf("Task: %s\n", p->name);
 		if (p->init) p->init(p->arg);
 	}
 	for (p = __task_begin; p < __task_end; p++)
 		p->release += now + 2;
-	return sys_timer_enqueue_tick(HZ/100, NULL);
-	return 0;
+	next = __find_next_task();
+	return sys_timer_enqueue_tick(next->release - now, NULL);
 }
 
-static int pts_handle(struct event_handler_data *d)
+task_initcall(pts_init);
+
+static void pts_handle(struct event_handler_data *d)
 {
-	struct bathos_task *p;
-	struct bathos_task *t;
-
-	for (t = p = __task_begin; p < __task_end; p++)
-		if (p->release < t->release)
-			t = p;
-	if (time_before(jiffies, t->release))
-		goto end;
-	t->arg = t->job(t->arg);
-	t->release += t->period;
-end:
-	return sys_timer_enqueue_tick(HZ/100, NULL);
-	return 0;
+	struct bathos_task *next;
+	do {
+		next = __find_next_task();
+		if (time_before(jiffies, next->release))
+			break;
+		next->arg = next->job(next->arg);
+		next->release += next->period;
+	} while(1);
+	sys_timer_enqueue_tick(next->release - jiffies, NULL);
 }
 
-declare_event_handler(sys_timer_tick, pts_init, pts_handle, NULL);
+declare_event_handler(sys_timer_tick, NULL, pts_handle, NULL);
