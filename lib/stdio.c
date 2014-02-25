a5735c39668b2b2027920832480dc01a332d9f27
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-02-25 17:57:08 +0100

    lib/stdio.c: add support for CONFIG_COPY_STDOUT_TO_CONSOLE

--- lib/stdio.c
+++ lib/stdio.c
@@ -5,6 +5,9 @@ struct bathos_pipe *bathos_stdin;
 
 void __attribute__((weak)) putc(int c)
 {
+#ifdef CONFIG_COPY_STDOUT_TO_CONSOLE
+	console_putc(c);
+#endif
 	if (!bathos_stdout)
 		return;
 	pipe_write(bathos_stdout, (char *)&c, 1);
