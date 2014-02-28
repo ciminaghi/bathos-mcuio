40054326db87d044994918f90fb21cfda0b210e1
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-03-01 00:09:43 +0100

    periodic_scheduler: simplify.
    We don't need to know what the next task is, we just reschedule each task
    p->period ticks after the current release, the system timer code will do
    the job of managing a delta list for us.

--- periodic_scheduler.c
+++ periodic_scheduler.c
@@ -8,43 +8,30 @@
 
 declare_event(sys_timer_tick);
 
-static struct bathos_task *__find_next_task(void)
-{
-	struct bathos_task *p;
-	struct bathos_task *t;
-	for (t = p = __task_begin; p < __task_end; p++)
-		if (p->release < t->release)
-			t = p;
-	return t;
-}
-
 static int pts_init(void)
 {
 	unsigned long now = jiffies;
-	struct bathos_task *p, *next;
+	struct bathos_task *p;
 	for (p = __task_begin; p < __task_end; p++) {
 		printf("Task: %s\n", p->name);
 		if (p->init) p->init(p->arg);
 	}
-	for (p = __task_begin; p < __task_end; p++)
-		p->release += now + 2;
-	next = __find_next_task();
-	return sys_timer_enqueue_tick(next->release - now, NULL);
+	for (p = __task_begin; p < __task_end; p++) {
+		p->release += now + 100;
+		if (sys_timer_enqueue_tick(p->release - now, p) < 0)
+			return -1;
+	}
+	return 0;
 }
 
 rom_initcall(pts_init);
 
 static void pts_handle(struct event_handler_data *d)
 {
-	struct bathos_task *next;
-	do {
-		next = __find_next_task();
-		if (time_before(jiffies, next->release))
-			break;
-		next->arg = next->job(next->arg);
-		next->release += next->period;
-	} while(1);
-	sys_timer_enqueue_tick(next->release - jiffies, NULL);
+	struct bathos_task *p = d->data;
+	p->arg = p->job(p->arg);
+	p->release += p->period;
+	sys_timer_enqueue_tick(p->release - jiffies, p);
 }
 
 declare_event_handler(sys_timer_tick, NULL, pts_handle, NULL);
