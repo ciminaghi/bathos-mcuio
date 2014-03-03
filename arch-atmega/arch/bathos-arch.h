e939d0fb7208f2b7c00c6c48b1eb2dc38f2c2275
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-03-03 09:51:23 +0100

    CHECK THIS: redefine time_after() and time_after_eq() for the atmega
    This should be fixed. time_after() doesn't __seem__ to work for the atmega.

--- arch-atmega/arch/bathos-arch.h
+++ arch-atmega/arch/bathos-arch.h
@@ -12,4 +12,10 @@
 #define interrupt_restore(flags) \
     SREG = flags
 
+#define time_after(a,b)		\
+    ((int)(b) - (int)(a) < 0)
+
+#define time_after_eq(a,b)	\
+    (((int)(a) - (int)(b)) >= 0)
+
 #endif /* __BATHOS_ARCH_H__ */
