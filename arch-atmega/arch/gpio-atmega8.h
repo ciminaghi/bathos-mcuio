f4085615d54c2cfaa3625d2cd8a1b2ef6e7f9eda
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-03-06 17:17:08 +0100

    atmega: implement gpios

---
+++ arch-atmega/arch/gpio-atmega8.h
@@ -0,0 +1,10 @@
+#ifndef __GPIO_ATMEGA8_H__
+#define __GPIO_ATMEGA8_H__
+
+#define ATMEGA_PORTB 2
+#define ATMEGA_PORTC 1
+#define ATMEGA_PORTD 0
+
+#define PORTS_BASE 0x10
+
+#endif /* __GPIO_ATMEGA8_H__ */
--- arch-atmega/arch/gpio.h
+++ arch-atmega/arch/gpio.h
@@ -1,35 +1,134 @@
 /*
- * Empty GPIO definitions, to allow libraries to be built
- * (then, clearly, we can't link with those functions, like w1-gpio)
+ * ATMEGA gpio definitions
  */
-#ifndef __GPIO_EMPTY__
-#define __GPIO_EMPTY__
+#ifndef __ATMEGA_GPIO_H__
+#define __ATMEGA_GPIO_H__
 
-extern void __no_gpio_code_for_this_architecture__(); /* link error */
+#include <bathos/types.h>
+#include <bathos/errno.h>
+#include <generated/autoconf.h>
+#include <avr/io.h>
 
-static inline int gpio_dir_af(int gpio, int output, int value, int afnum)
+#if defined CONFIG_MACH_ATMEGA32U4
+#include <arch/gpio-atmega32u4.h>
+#endif
+#if defined CONFIG_MACH_ATMEGA8
+#include <arch/gpio-atmega8.h>
+#endif
+
+/* Can handle 8 gpios belonging to the same port with __gpio_set_port */
+#define HAVE_GPIO_PORT_8
+
+#define GPIO_PORT_WIDTH 8
+#define GPIO_NR(port, bit) ((port) * GPIO_PORT_WIDTH + (bit))
+#define GPIO_PORT(nr)      ((nr) / GPIO_PORT_WIDTH)
+#define GPIO_BIT(nr)       ((nr) % GPIO_PORT_WIDTH)
+
+static inline void gpio_set(int gpio, int value)
 {
-	__no_gpio_code_for_this_architecture__();
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	int addr = PORTS_BASE + port * 3 + 2;
+	if (value)
+		_SFR_IO8(addr) |= (1 << pin);
+	else
+		_SFR_IO8(addr) &= ~(1 << pin);
+}
+
+static inline void __gpio_set(int gpio, u32 value)
+{
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	int addr = PORTS_BASE + port * 3 + 2;
+	if (value)
+		_SFR_IO8(addr) |= (u8)value;
+	else
+		_SFR_IO8(addr) &= ~(1 << pin);
+}
+
+static inline int __gpio_set_port8(int gpio, u8 value)
+{
+	int port = GPIO_PORT(gpio);
+	int addr = PORTS_BASE + port * 3 + 2;
+	if (gpio % GPIO_PORT_WIDTH)
+		return -EINVAL;
+	_SFR_IO8(addr) = (u8)value;
 	return 0;
 }
 
 static inline void gpio_dir(int gpio, int output, int value)
 {
-	__no_gpio_code_for_this_architecture__();
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	int data_addr = PORTS_BASE + port * 3 + 2;
+	int dir_addr = PORTS_BASE + port * 3 + 1;
+	uint8_t mask = (1 << pin);
+	if (value)
+		_SFR_IO8(data_addr) |= mask;
+	else
+		_SFR_IO8(data_addr) &= ~mask;
+	if (output)
+		_SFR_IO8(dir_addr) |= mask;
+	else
+		_SFR_IO8(dir_addr) &= ~mask;
 }
 
 static inline int gpio_get(int gpio)
 {
-	__no_gpio_code_for_this_architecture__();
-	return -1;
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	uint8_t mask = (1 << pin);
+	int addr = PORTS_BASE + port * 3;
+	return _SFR_IO8(addr) & mask ? 1 : 0;
 }
-#define __gpio_get gpio_get
 
-static inline void gpio_set(int gpio, int value)
+static inline u32 __gpio_get(int gpio)
 {
-	__no_gpio_code_for_this_architecture__();
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	int addr = PORTS_BASE + port * 3;
+	return _SFR_IO8(addr) & (1 << pin);
 }
-#define __gpio_set gpio_set
+
+static inline u8 __gpio_get_port8(int gpio)
+{
+	int port = GPIO_PORT(gpio);
+	int addr = PORTS_BASE + port * 3;
+	if (gpio % GPIO_PORT_WIDTH)
+		return -EINVAL;
+	return _SFR_IO8(addr);
+}
+
+static inline int gpio_dir_af(int gpio, int output, int value, int afnum)
+{
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	int addr = PORTS_BASE + port * 3 + 1;
+	if (afnum != 0)
+		/* Unsupported at the moment */
+		return -EINVAL;
+	gpio_set(gpio, value);
+	if (output)
+		_SFR_IO8(addr) |= (1 << pin);
+	else
+		_SFR_IO8(addr) &= ~(1 << pin);
+	return 0;
+}
+
+static inline int gpio_get_dir_af(int gpio, int *output, int *value, int *afnum)
+{
+	int port = GPIO_PORT(gpio);
+	int pin = GPIO_BIT(gpio);
+	int addr = PORTS_BASE + port * 3 + 1;
+	if (afnum)
+		/* Unsupported at the moment */
+		*afnum = 0;
+	if (value)
+		*value = gpio_get(gpio);
+	if (*output)
+		*output = _SFR_IO8(addr) & (1 << pin) ? 1 : 0;
+	return 0;
+}
 
 
-#endif /* __GPIO_EMPTY__ */
+#endif /* __ATMEGA_GPIO_H__ */
