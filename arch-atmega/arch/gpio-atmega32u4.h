b651b919504cc8be8a58111e1bdaa287eb0a0226
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-03-06 17:17:08 +0100

    atmega: implement gpios

---
+++ arch-atmega/arch/gpio-atmega32u4.h
@@ -0,0 +1,12 @@
+#ifndef __GPIO_ATMEGA32U4_H__
+#define __GPIO_ATMEGA32U4_H__
+
+#define ATMEGA_PORTB 0
+#define ATMEGA_PORTC 1
+#define ATMEGA_PORTD 2
+#define ATMEGA_PORTE 3
+#define ATMEGA_PORTF 4
+
+#define PORTS_BASE 0x03
+
+#endif /* __GPIO_ATMEGA32U4_H__ */
