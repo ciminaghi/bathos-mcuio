58862ff5fcc58f52a580d27d0fe9a4044a351058
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-04-08 11:57:29 +0200

    mcuio gpio: add events support

--- tasks/mcuio_gpio_func.c
+++ tasks/mcuio_gpio_func.c
@@ -3,9 +3,16 @@
 #include <bathos/bathos.h>
 #include <bathos/pipe.h>
 #include <bathos/errno.h>
+#include <bathos/bitops.h>
+#include <bathos/string.h>
 #include <tasks/mcuio.h>
 
 #include "mcuio-function.h"
+
+extern struct mcuio_function gpio;
+declare_extern_event(gpio_evt);
+declare_extern_event(mcuio_irq);
+
 
 extern unsigned int PROGMEM gpio_labels_size;
 extern const char PROGMEM gpio_labels_start[];
@@ -36,7 +43,10 @@ static const struct mcuio_func_descriptor PROGMEM gpio_descr = {
 static const unsigned int PROGMEM gpio_descr_length = sizeof(gpio_descr);
 
 /* FIXME !! Does not work in case of multiple instances */
-static uint64_t gpio_events_status;
+static uint32_t gpio_events_status[2];
+
+static uint32_t gpio_events_falling[2];
+static uint32_t gpio_events_rising[2];
 
 static uint32_t PROGMEM gpio_ro_range1[] = {
 	/* FIXME: MAKE THIS CONFIGURABLE !! */
@@ -362,6 +372,100 @@ const struct mcuio_range_ops PROGMEM gpio_modes_ops = {
 		gpio_modes_wrq, },
 };
 
+static int __gpio_evts_rd(const struct mcuio_range *r, unsigned offset,
+			  void *__out, int fill, int width)
+{
+	int start = offset, n = width/8, i, ret;
+	uint8_t *out = __out;
+	if (fill)
+		n = min(MCUIO_NGPIO, sizeof(uint64_t)/8);
+	ret = n;
+	for (i = 0; i < n && ret > 0; i++, out++) {
+		*out = 0;
+		if (test_bit(i + start, gpio_events_falling))
+			*out |= 2;
+		if (test_bit(i + start, gpio_events_rising))
+			*out |= 1;
+	}
+	return ret;
+}
+
+static int gpio_evts_rddw(const struct mcuio_range *r, unsigned offset,
+			   uint32_t *__out, int fill)
+{
+	return __gpio_evts_rd(r, offset, __out, fill, 32);
+}
+
+static int __gpio_evts_wr(const struct mcuio_range *r, unsigned offset,
+			  const void *__in, int fill, int width)
+{
+	int start = offset, n = width/8, i, ret, stat;
+	const uint8_t *in = __in;
+	if (fill)
+		n = min(MCUIO_NGPIO, sizeof(uint64_t)/8);
+	ret = n;
+	for (i = 0; i < n; i++, in++) {
+		if (*in & 2)
+			set_bit(i + start, gpio_events_falling);
+		if (*in & 1)
+			set_bit(i + start, gpio_events_rising);
+		stat = gpio_request_events(i + start, *in);
+		if (stat < 0)
+			return stat;
+			
+	}
+	return ret;
+}
+
+static int gpio_evts_wrdw(const struct mcuio_range *r, unsigned offset,
+			  const uint32_t *__in, int fill)
+{
+	return __gpio_evts_wr(r, offset, __in, fill, 32);
+}
+
+const struct mcuio_range_ops PROGMEM gpio_evts_ops = {
+	.rd = { NULL, NULL, gpio_evts_rddw, NULL, },
+	.wr = { NULL, NULL, gpio_evts_wrdw, NULL, },
+};
+
+static int __gpio_evts_status_rd(const struct mcuio_range *r, unsigned offset,
+				 void *__out, int fill, int width)
+{
+	int start = offset, n = width/8, i, ret;
+	static struct mcuio_function_irq_data idata;
+	uint32_t prev_gpio_events_status[2];
+	uint8_t *out = __out, *ptr = &((uint8_t *)gpio_events_status)[start];
+	if (fill)
+		n = min(MCUIO_NGPIO, sizeof(uint64_t)/8);
+	ret = n;
+	memcpy(prev_gpio_events_status, gpio_events_status,
+	       sizeof(gpio_events_status));
+	for (i = 0; i < n && ret > 0; i++, out++, ptr++) {
+		/* Read and automatically clear */
+		*out = *ptr;
+		*ptr = 0;
+	}
+	if ((prev_gpio_events_status[0] || prev_gpio_events_status[1]) &&
+	    (!gpio_events_status[0] && !gpio_events_status[1])) {
+		idata.func = &gpio - mcuio_functions_start;
+		idata.active = 0;
+		if (trigger_event(&event_name(mcuio_irq), &idata, EVT_PRIO_MAX))
+			printf("%s: evt error\n", __func__);
+	}
+	return ret;
+}
+
+static int gpio_evts_status_rddw(const struct mcuio_range *r, unsigned offset,
+				 uint32_t *__out, int fill)
+{
+	return __gpio_evts_status_rd(r, offset, __out, fill, 32);
+}
+
+const struct mcuio_range_ops PROGMEM gpio_evts_status_ops = {
+	.rd = { NULL, NULL, gpio_evts_status_rddw, NULL, },
+	.wr = { NULL, NULL, NULL, NULL, },
+};
+
 static const struct mcuio_range PROGMEM gpio_ranges[] = {
 	/* GPIO func descriptor */
 	{
@@ -415,7 +519,7 @@ static const struct mcuio_range PROGMEM gpio_ranges[] = {
 		.length = &gpio_evts_masks_size,
 		.rd_target = NULL,
 		.wr_target = NULL,
-		.ops = NULL,
+		.ops = &gpio_evts_ops,
 	},
 	/* dwords 0x910 0x914, gpio data */
 	{
@@ -435,24 +539,42 @@ static const struct mcuio_range PROGMEM gpio_ranges[] = {
 	},
 	/* dwords 0x920 0x924, gpio clear */
 	{
-		.start = 0x918,
+		.start = 0x920,
 		.length = &gpio_data_length,
 		.rd_target = NULL,
 		.wr_target = NULL,
 		.ops = &gpio_clr_ops,
 	},
 	/*
-	  dwords 0x918 0x91c gpio events status, currently
+	  dwords 0x928 0x92c gpio events status, currently
 	  not implemented
 	*/
 	{
-		.start = 0x918,
+		.start = 0x928,
 		.length = &gpio_evts_status_length,
-		.rd_target = &gpio_events_status,
-		.wr_target = &gpio_events_status,
-		.ops = NULL,
+		.rd_target = gpio_events_status,
+		.ops = &gpio_evts_status_ops,
 	},
 };
 
 
 declare_mcuio_function(gpio, gpio_ranges, NULL, NULL, &gpio_rt);
+
+declare_event(mcuio_irq);
+
+static void gpio_evt_handle(struct event_handler_data *ed)
+{
+	uint32_t *status = ed->data;
+	static struct mcuio_function_irq_data idata;
+	gpio_events_status[0] |= status[0];
+	status[0] = 0;
+	gpio_events_status[1] |= status[1];
+	status[1] = 0;
+	if (!gpio_events_status[0] && !gpio_events_status[1])
+		printf("NULL gpio event\n");
+	idata.func = &gpio - mcuio_functions_start;
+	idata.active = 1;
+	trigger_event(&event_name(mcuio_irq), &idata, EVT_PRIO_MAX);
+}
+
+declare_event_handler(gpio_evt, NULL, gpio_evt_handle, NULL);
