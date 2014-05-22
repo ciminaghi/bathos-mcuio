0fbcd5b25d8d658181f03e965c8c3e6d92efc91a
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-05-23 00:37:42 +0200

    atmega: add functions for dealing with program->data memory copy

--- arch-atmega/arch/bathos-arch.h
+++ arch-atmega/arch/bathos-arch.h
@@ -1,7 +1,10 @@
 #ifndef __BATHOS_ARCH_H__
 #define __BATHOS_ARCH_H__
 
+#include <stddef.h>
+#include <bathos/string.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 
 #define interrupt_disable(flags) \
     do {			 \
@@ -17,5 +20,28 @@
 
 #define time_after_eq(a,b)	\
     (((int)(a) - (int)(b)) >= 0)
+
+static inline int __copy_byte(uint8_t *dst, const uint8_t *src)
+{
+	*dst = pgm_read_byte_near(src);
+	return 0;
+}
+
+static inline int __copy_word(uint16_t *dst, const uint16_t *src)
+{
+	*dst = pgm_read_word_near(src);
+	return 0;
+}
+
+static inline int __copy_dword(uint32_t *dst, const uint32_t *src)
+{
+	*dst = pgm_read_dword_near(src);
+	return 0;
+}
+
+static inline void *memcpy_p(void *dst, const void *src, int size)
+{
+	return memcpy_P(dst, src, size);
+}
 
 #endif /* __BATHOS_ARCH_H__ */
