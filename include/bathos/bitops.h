93a03be1bfb5cee7a2ac79d94df8308420c9cfb6
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-06-27 16:42:42 +0200

    bitops: take BITS_PER_LONG define as default only
    unix x86_64 arch will have a different BITS_PER_LONG

--- include/bathos/bitops.h
+++ include/bathos/bitops.h
@@ -4,7 +4,9 @@
 //#include <asm/types.h>
 #include <bathos/interrupt.h>
 
+#ifndef BITS_PER_LONG
 #define BITS_PER_LONG		32
+#endif
 #define BIT(nr)			(1UL << (nr))
 #define BIT_ULL(nr)		(1ULL << (nr))
 #define BIT_MASK(nr)		(1UL << ((nr) % BITS_PER_LONG))
