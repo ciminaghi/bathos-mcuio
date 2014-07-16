fedfb0aedccc0558b41d5bb84a25efd766551725
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-07-16 05:30:33 +0200

    lib/statemachine: put tables in flash and save memory

--- include/bathos/statemachine.h
+++ include/bathos/statemachine.h
@@ -1,6 +1,8 @@
 #ifndef __STATEMACHINE_H__
 #define __STATEMACHINE_H__
 
+#include <arch/bathos-arch.h>
+
 struct statemachine_state;
 struct statemachine_runtime;
 
@@ -9,36 +11,41 @@ enum statemachine_type {
 	MEALEY,
 };
 
-typedef void (state_outfunc)(const struct statemachine_state *,
+typedef void (state_outfunc)(const struct statemachine_state * PROGMEM,
 			     struct statemachine_runtime *);
+typedef state_outfunc *pstate_outfunc;
 
 struct statemachine_state {
 	/* There shall be as many next states as are the valid events */
-	const int *next_states;
+	const int * PROGMEM next_states;
 	/* 
 	 * This array contains one entry for moore machines, one entry per
 	 * valid event for mealey machines
 	 */
-	state_outfunc **out;
+	state_outfunc * const * PROGMEM out;
 };
 
 struct statemachine_runtime {
 	int curr_state;
-	void *priv;
 };
+
+static inline int statemachine_get_state(struct statemachine_runtime *r)
+{
+	return r->curr_state;
+}
 
 struct statemachine {
 	enum statemachine_type type;
 	int nstates;
-	const struct statemachine_state *states;
+	const struct statemachine_state * PROGMEM states;
 	int nevents;
-	/* We may have more instances of the same state machine */
-	struct statemachine_runtime *runtimes;
-	int nruntimes;
 };
 
-extern int init_statemachine(const struct statemachine *, int initial_state);
-extern int feed_statemachine(const struct statemachine *,
-			     int instance, int event);
+extern int init_statemachine(const struct statemachine * PROGMEM,
+			     struct statemachine_runtime *runtime,
+			     int initial_state);
+extern int feed_statemachine(const struct statemachine * PROGMEM,
+			     struct statemachine_runtime *runtime,
+			     int event);
 
 #endif /* __STATEMACHINE_H__ */
--- lib/statemachine.c
+++ lib/statemachine.c
@@ -2,38 +2,101 @@
 #include <bathos/errno.h>
 #include <bathos/statemachine.h>
 
+#if defined ARCH_IS_HARVARD
 
-int init_statemachine(const struct statemachine *m, int initial_state)
+static struct statemachine *
+__get_statemachine(const struct statemachine * PROGMEM _m,
+		   struct statemachine *m)
 {
-	int i;
+	memcpy_p(m, _m, sizeof(*m));
+	return m;
+}
+
+static struct statemachine_state *
+__get_state(struct statemachine *m, int sn, struct statemachine_state *s)
+{
+	memcpy_p(s, &m->states[sn], sizeof(*s));
+	return s;
+}
+
+static int __get_next(struct statemachine_state *s, int e)
+{
+	int out;
+	__copy_int(&out, &s->next_states[e]);
+	return out;
+}
+
+static pstate_outfunc __get_outf(struct statemachine_state *s, int e)
+{
+	pstate_outfunc out;
+	memcpy_p(&out, &s->out[e], sizeof(out));
+	return out;
+}
+
+#else
+
+static struct statemachine *
+__get_statemachine(const struct statemachine * PROGMEM _m,
+		   struct statemachine *m)
+{
+	return _m;
+}
+
+static struct statemachine_state *
+__get_state(struct statemachine *m, int sn, struct statemachine_state *s)
+{
+	return &m->states[sn];
+}
+
+static int __get_next(struct statemachine_state *s, int e)
+{
+	return s->next_states[e];
+}
+
+static pstate_outfunc __get_outf(struct statemachine_state *s, int e)
+{
+	return &s->out[e];
+}
+
+#endif
+
+
+int init_statemachine(const struct statemachine * PROGMEM _m,
+		      struct statemachine_runtime *rt,
+		      int initial_state)
+{
+	struct statemachine *m, __m;
+
+	m = __get_statemachine(_m, &__m);
 	if (m->nstates <= 0 || !m->states || !m->nevents)
 		return -EINVAL;
 	if (initial_state < 0 || initial_state >= m->nstates)
 		return -EINVAL;
-	for (i = 0; i < m->nruntimes; i++)
-		m->runtimes[i].curr_state = initial_state;
+	rt->curr_state = initial_state;
 	return 0;
 }
 
-int feed_statemachine(const struct statemachine *m, int instance, int event)
+int feed_statemachine(const struct statemachine * PROGMEM _m,
+		      struct statemachine_runtime *rt,
+		      int event)
 {
-	const struct statemachine_state *next;
-	const struct statemachine_state *c;
-	state_outfunc *outf;
-	struct statemachine_runtime *runtime;
+	struct statemachine *m, __m;
+	struct statemachine_state *next, __next;
+	struct statemachine_state *c, __c;
+	pstate_outfunc outf;
 
+	m = __get_statemachine(_m, &__m);
 	if (event < 0 || event >= m->nevents)
 		return -EINVAL;
-	if (instance < 0 || instance >= m->nruntimes)
+	if (!rt)
 		return -EINVAL;
-	runtime = &m->runtimes[instance];
-	c = &m->states[runtime->curr_state];
-	next = &m->states[c->next_states[event]];
+	c = __get_state(m, rt->curr_state, &__c);
+	next = __get_state(m, __get_next(c, event), &__next);
 	if (!next)
 		return -EINVAL;
-	runtime->curr_state = next - m->states;
-	outf = m->type == MOORE ? next->out[0] : c->out[event];
+	rt->curr_state = __get_next(c, event);
+	outf = m->type == MOORE ? __get_outf(next, 0) : __get_outf(c, event);
 	if (outf)
-		outf(next, runtime);
+		outf(next, rt);
 	return 0;
 }
