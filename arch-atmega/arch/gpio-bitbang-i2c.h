b760745ed81aa07b9fec1e24a68099698a5d9716
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-09-24 12:19:24 +0200

    arch-atmega: add special functions for handling i2c signals (bitbang driver)

---
+++ arch-atmega/arch/gpio-bitbang-i2c.h
@@ -0,0 +1,48 @@
+#ifndef __GPIO_BITBANG_H__
+#define __GPIO_BITBANG_H__
+
+#define setsda __atmega_bitbang_setsda
+#define setscl __atmega_bitbang_setscl
+#define getscl __atmega_bitbang_getscl
+#define getsda __atmega_bitbang_getsda
+
+
+static inline void __atmega_bitbang_setsda(int v)
+{
+	/* SDA -> output */
+	DDRD |= _BV(DDD1);
+	/* Set value */
+	if (v) {
+		PORTD |= _BV(PORTD1);
+		/* Make it an input again */
+		DDRD &= ~_BV(DDD1);
+		return;
+	}
+	PORTD &= ~_BV(PORTD1);
+}
+
+static inline void __atmega_bitbang_setscl(int v)
+{
+	/* SCL -> output */
+	DDRD |= _BV(DDD0);
+	/* Set value */
+	if (v) {
+		PORTD |= _BV(PORTD0);
+		/* Make it an input again */
+		DDRD &= ~_BV(DDD0);
+		return;
+	}
+	PORTD &= ~_BV(PORTD0);
+}
+
+static inline int __atmega_bitbang_getscl(void)
+{
+	return PIND & _BV(PIND0);
+}
+
+static inline int __atmega_bitbang_getsda(void)
+{
+	return PIND & _BV(PIND1);
+}
+
+#endif /* __GPIO_BITBANG_H__ */
