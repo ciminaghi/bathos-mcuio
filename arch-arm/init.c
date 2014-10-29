4a58596840c49df32558f6466bfb36c429f0fefb
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-10-29 17:14:16 +0100

    ARM: add basic skeleton

---
+++ arch-arm/init.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Davide Ciminaghi <ciminaghi@gnudd.com>
+ */
+
+#include <bathos/bathos.h>
+#include <bathos/io.h>
+#include <bathos/init.h>
+#include <bathos/pipe.h>
+#include <bathos/shell.h>
+#include <generated/autoconf.h>
+#include <arch/hw.h>
+#include <stdint.h>
+
+volatile unsigned long __attribute__((weak)) jiffies;
+
+/* Dummy start function (replaced by machine's romboot_start in boot.S */
+void __attribute__((weak)) _romboot_start(void)
+{
+}
+
+int stdio_init(void)
+{
+	return 0;
+}
+core_initcall(stdio_init);
+
+int bathos_setup(void)
+{
+	return 0;
+}
