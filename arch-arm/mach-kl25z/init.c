a75c20a3a81c32de5e1631bd9ca5dde8fdd725c2
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-11-12 00:02:45 +0100

    KL25Z: make mach_ll_init a rom_initcall
    This will let us avoid invoking mach_ll_init for ram startup (the hw should
    already have been initialized).

--- arch-arm/mach-kl25z/init.c
+++ arch-arm/mach-kl25z/init.c
@@ -1,7 +1,26 @@
 
 #include <arch/hw.h>
+#include <bathos/gpio.h>
+#include <bathos/init.h>
 
-void mach_ll_init(void)
+unsigned long jiffies;
+
+#ifdef CONFIG_CONSOLE_DEBUG_BITBANG
+static void console_gpio_init(void)
+{
+	gpio_dir_af(CONFIG_BB_CLOCK_GPIO, 1, 1, 1);
+	gpio_dir_af(CONFIG_BB_DATA_GPIO, 1, 1, 1);
+}
+#else
+static void console_gpio_init(void)
+{
+}
+#endif
+
+static int mach_ll_init(void)
 {
 	clocks_init();
+	console_gpio_init();
+	return 0;
 }
+rom_initcall(mach_ll_init);
