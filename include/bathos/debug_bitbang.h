ea03d44579543a3e5e779ca9ddf03cba3212123a
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-11-11 11:51:54 +0100

    drivers: add debug bitbang pipe driver

---
+++ include/bathos/debug_bitbang.h
@@ -0,0 +1,11 @@
+#ifndef __DEBUG_BITBANG_H__
+#define __DEBUG_BITBANG_H__
+
+
+#include <bathos/bathos.h>
+#include <bathos/pipe.h>
+
+extern struct bathos_dev_ops PROGMEM bitbang_dev_ops;
+
+
+#endif
