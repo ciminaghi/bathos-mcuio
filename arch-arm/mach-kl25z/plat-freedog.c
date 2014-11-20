09161622f0735faa25f1e7a824a6b2d10b9ed7ef
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2014-11-20 15:25:58 +0100

    arch-arm: new (almost empty) plat-freedog.c file
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

---
+++ arch-arm/mach-kl25z/plat-freedog.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
+ */
+
+#define VRES_UV (5000000 / (1 << 10))
+
+#ifdef CONFIG_MBED_DRIVERS
+
+/* FIXME put mbed-related platform data */
+
+#endif
