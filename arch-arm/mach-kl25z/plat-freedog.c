4339af4604e35f79c39e57e4a3de44e0650840f0
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2014-11-27 11:43:38 +0100

    TEMPORARY: first implementation of mbed adc driver for kl25z
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch-arm/mach-kl25z/plat-freedog.c
+++ arch-arm/mach-kl25z/plat-freedog.c
@@ -4,10 +4,25 @@
  * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
  */
 
+/* ADCs */
+
 #define VRES_UV (5000000 / (1 << 10))
 
 #ifdef CONFIG_MBED_DRIVERS
 
-/* FIXME put mbed-related platform data */
+#include <analogin_api.h>
+
+const struct adc adcs[] = {
+	BATHOS_ADC((uint8_t)PTE30, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A0 */
+	BATHOS_ADC((uint8_t)PTE29, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A1 */
+	BATHOS_ADC((uint8_t)PTE24, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A2 */
+	BATHOS_ADC((uint8_t)PTE25, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A3 */
+	BATHOS_ADC((uint8_t)PTE21, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A4 */
+	BATHOS_ADC((uint8_t)PTE20, BATHOS_ADC_FLAG_NONE, VRES_UV), /* A5 */
+};
+
+const uint32_t num_adc = ARRAY_SIZE(adcs);
+const uint32_t min_per = (1000000 / HZ);
+const uint32_t max_mul = 0xffffffff;
 
 #endif
