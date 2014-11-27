ebaf88aba7a3c83866cc8268264c049410498a8d
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2014-11-28 00:19:03 +0100

    ARM: new api for cortex-m0 NVIC
    API for nvic handling, including nvic in ram.
    A new ram_vectors variable needs to be defined in bathos.lds, defining
    the place where to put ram nvics.
    
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

---
+++ arch-arm/cpu-cortex-m0/nvic.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
+ */
+
+#include "hw.h"
+
+#ifndef ___CORTEX_M0_NVIC_H__
+#define ___CORTEX_M0_NVIC_H__
+
+extern volatile uint32_t rom_vectors[];
+extern volatile uint32_t ram_vectors[];
+extern volatile uint32_t *vectors;
+
+static inline void nvic_init_ram()
+{
+	int i;
+	for (i = 0; i < NVIC_CORE_NUM + NVIC_USER_NUM; i++)
+		ram_vectors[i] = rom_vectors[i];
+	vectors = ram_vectors;
+	regs[REG_SCB_VTOR] = (uint32_t)vectors;
+}
+
+static inline void nvic_set_handler(uint32_t irqn, void (*irq_handler)(void))
+{
+	vectors[irqn + NVIC_CORE_NUM] = (uint32_t)irq_handler;
+}
+
+static inline void nvic_enable(uint32_t irqn)
+{
+	regs[REG_NVIC_ISER] = 1 << irqn;
+}
+
+static inline void nvic_disable(uint32_t irqn)
+{
+	regs[REG_NVIC_ICER] = 1 << irqn;
+}
+
+#endif /* ___CORTEX_M0_NVIC_H__ */
