ff296d7c599a91c3ba76a0449a1b522149d677c3
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2014-11-28 12:14:49 +0100

    CORTEX-M0: hw.h file, with NVIC and SCB
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

---
+++ arch-arm/cpu-cortex-m0/hw.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
+ */
+
+/* cortex-m0 registers */
+
+#define REG_NVIC_ISER		(0xe000e100 / 4)
+#define REG_NVIC_ICER		(0xe000e180 / 4)
+#define REG_NVIC_ISPR		(0xe000e200 / 4)
+#define REG_NVIC_ICPR		(0xe000e280 / 4)
+#define REG_NVIC_IP(prio)	((0xe000e400 + (prio)*0x4) / 4)
+
+#define REG_SCB_CPUID		(0xe000ed00 / 4)
+#define REG_SCB_ICSR		(0xe000ed04 / 4)
+#define REG_SCB_VTOR		(0xe000ed08 / 4)
+#define REG_SCB_AIRCR		(0xe000ed0c / 4)
+#define REG_SCB_SCR		(0xe000ed10 / 4)
+#define REG_SCB_CCR		(0xe000ed14 / 4)
+#define REG_SCB_SHP2		(0xe000ed1c / 4)
+#define REG_SCB_SHP3		(0xe000ed20 / 4)
+
+/* Interrupt Vector */
+#define NVIC_CORE_NUM		16
+#define NVIC_USER_NUM		32
+#define NVIC_IRQ_NMI 		( 2 - NVIC_CORE_NUM)
+#define NVIC_IRQ_HARD_FAULT 	( 3 - NVIC_CORE_NUM)
+#define NVIC_IRQ_SVCALL 	(11 - NVIC_CORE_NUM),
+#define NVIC_IRQ_PENDSV		(14 - NVIC_CORE_NUM),
+#define NVIC_IRQ_SYSTICK	(15 - NVIC_CORE_NUM),
