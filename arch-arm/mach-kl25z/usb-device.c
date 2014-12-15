f9e0d88c67581d6b46a1c361444b886cbdc4ad09
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2014-12-15 11:26:41 +0100

    KL25Z/USB driver: actually implemented cdc-acm profile
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch-arm/mach-kl25z/usb-device.c
+++ arch-arm/mach-kl25z/usb-device.c
@@ -6,6 +6,7 @@
 
 /* Generic driver for usb device */
 
+/* #define DEBUG 1 */
 #include <mach/hw.h>
 #include <arch/hw.h>
 #include <arch/bathos-arch.h>
@@ -23,7 +24,9 @@
 extern struct usb_ops_t __usb_ops;
 
 volatile uint8_t *regs8 = (void*)regs;
-static uint8_t ep0_rx_buf[2 * EP0_BUFSIZE];
+static uint8_t ep0_rx_buf[2 * EP0_BUFSIZE] __attribute__ ((aligned(4)));
+
+static int __usb_flush(uint8_t ep);
 
 struct bdt_t {
 	uint8_t flags;
@@ -48,8 +51,26 @@ struct usb_device_t {
 	struct bdt_t *bdt;
 };
 
+struct endpoint_t {
+	int rx_bufsize;
+	int tx_pktlen;
+	const uint8_t *tx_pend;
+	int tx_pend_len;
+	uint8_t tx_data0;
+	uint8_t tx_odd;
+	uint8_t tx_valid:1;
+};
+
 static struct usb_device_t __usb;
 static struct usb_device_t *usb = &__usb;
+
+/* Minimal bdt usb configuration valid for a usb serial
+ * Warning: increase this if you need more endpoints in your driver */
+#ifndef USB_EP_NUM
+#define USB_EP_NUM 4
+#endif
+static uint8_t bdt[USB_EP_NUM * 8 * 2 * 2] __attribute__((__aligned__(512)));
+static struct endpoint_t eps[USB_EP_NUM];
 
 int usb_set_bdt(void *bdt, unsigned int nep)
 {
@@ -71,12 +92,6 @@ int usb_set_bdt(void *bdt, unsigned int nep)
 	return 0;
 }
 
-/* Minimal bdt usb configuration valid for a usb serial */
-#ifndef USB_EP_NUM
-#define USB_EP_NUM 4
-#endif
-static uint8_t bdt[USB_EP_NUM * 8 * 2 * 2] __attribute__((__aligned__(512)));
-
 static int __usb_init(void)
 {
 	regs[REG_SOPT2] |= SIM_SOPT2_USBSRC_MASK;
@@ -87,8 +102,8 @@ static int __usb_init(void)
 
 core_initcall(__usb_init);
 
-static int usb_request_ep(uint8_t ep, int tx, uint8_t *buf1, uint8_t *buf2,
-			 int bufsize)
+static inline int usb_request_ep(uint8_t ep, int tx, uint8_t *buf1,
+				uint8_t *buf2, int bufsize)
 {
 	uint8_t idx;
 	struct bdt_t *bd;
@@ -97,19 +112,14 @@ static int usb_request_ep(uint8_t ep, int tx, uint8_t *buf1, uint8_t *buf2,
 
 	bd = &usb->bdt[idx];
 
-	if (bd->address)
-		goto done;
-
 	bd[0].address = (uint32_t)buf1;
-	bd[0].bc = bufsize;
+	bd[1].address = (uint32_t)buf2;
 
-	bd[0].address = (uint32_t)buf2;
 	bd[0].bc = bufsize;
+	bd[1].bc = bufsize;
 
-	if (!tx) {
-		bd[0].flags = OWN_MASK;
-		bd[1].flags = OWN_MASK;
-	}
+	bd[0].flags = tx ? 0: OWN_MASK;
+	bd[1].flags = bd[0].flags;
 
 	/* Enable endpoint with handshake bit set to 1 */
 	regs8[REG_USB0_ENDPT(ep)] |= (tx ? 0x05 : 0x09);
@@ -118,29 +128,34 @@ done:
 	return 0;
 }
 
-int usb_request_tx_ep(uint8_t ep)
+int usb_request_tx_ep(uint8_t ep, void *buf, int bufsize)
 {
-	return usb_request_ep(ep, 1, NULL, NULL, 0);
+	/* no internal buffer for tx: address will be set on usb_write */
+	eps[ep].tx_pktlen = bufsize;
+	eps[ep].tx_valid = 0;
+	eps[ep].tx_odd = 0;
+	eps[ep].tx_data0 = 0;
+	return usb_request_ep(ep, 1, NULL, NULL, bufsize);
 }
 
 int usb_request_rx_ep(uint8_t ep, void *buf, int bufsize)
 {
-	return usb_request_ep(ep, 0, buf, buf + bufsize, 0);
+	eps[ep].rx_bufsize = bufsize;
+	return usb_request_ep(ep, 0, buf, buf + bufsize, bufsize);
 }
 
-int usb_release_ep(uint8_t ep, int bufsize, int tx)
+int usb_release_tx_ep(uint8_t ep, int bufsize)
 {
-	regs8[REG_USB0_ENDPT(ep)] &= ~(tx ? 0x05 : 0x09);
+	regs8[REG_USB0_ENDPT(ep)] &= ~0x05;
 	return 0;
 }
 
-#define _to_u16(l, h) ((uint16_t)h << 8 | l)
-
-/* FIXME One for each EP */
-static uint8_t *_tx;
-static int _len;
-static uint8_t data0 = 0;
-static uint8_t odd = 0;
+int usb_release_rx_ep(uint8_t ep, int bufsize)
+{
+	regs8[REG_USB0_ENDPT(ep)] &= ~0x09;
+	eps[ep].rx_bufsize = 0;
+	return 0;
+}
 
 static uint8_t _usb_device_addr = 0;
 int usb_set_address(uint8_t addr)
@@ -154,15 +169,17 @@ static void __usb_irq_handler(void)
 	uint8_t mask;
 	uint8_t stat;
 	struct bdt_t *bd;
+	uint8_t ep;
 
 	mask = regs8[REG_USB0_ISTAT];
 	stat = regs8[REG_USB0_STAT];
 	bd =  &usb->bdt[stat >> 2];
+	ep = stat >> 4;
 
 	if (mask & USB_ISTAT_USBRST_MASK) {
 		regs8[REG_USB0_CTL] |= USB_CTL_ODDRST_MASK;
 		usb_request_rx_ep(0, ep0_rx_buf, EP0_BUFSIZE);
-		usb_request_tx_ep(0);
+		usb_request_tx_ep(0, NULL, EP0_BUFSIZE);
 		regs8[REG_USB0_ADDR] = 0x00;
 		regs8[REG_USB0_ISTAT] = 0xff;
 		if (__usb_ops.reset)
@@ -173,60 +190,23 @@ static void __usb_irq_handler(void)
 	if (mask & USB_ISTAT_TOKDNE_MASK) {
 		uint8_t pid = (bd->flags & TOK_PID_MASK) >> TOK_PID_SHIFT;
 
-		if (pid == USB_TOKEN_SETUP) {
-			int len;
-			int i;
-			struct usb_tok_setup *s =
-				(struct usb_tok_setup *)bd->address;
-			uint16_t s_wValue = _to_u16(s->wValueL, s->wValueH);
-			uint16_t s_wLength = _to_u16(s->wLengthL, s->wLengthH);
-			const struct usb_descriptor_list *d = NULL;
-
-			data0 = DATA01_MASK;
-
-			if (s->bRequest == 0x6) { /* get_descriptor */
-				for (i = 0; ; i++) {
-					d = &usb_descr_list[i];
-					if ((d->wValue == s_wValue) ||
-					    (!usb_descr_list[i].addr))
-						break;
-				}
-				if (!d->addr) {
-					pr_debug(
-					  "usb warning: unknown descr %02x\n",
-					  s_wValue);
-					usb_write(0, NULL, 0);
-					goto token_done;
-				}
-				if (d->length)
-					len = d->length;
-				else
-					len = d->addr[0];
-				usb_write(0, d->addr, min(len, s_wLength));
-			}
-			else if (s->bRequest == 0x5) { /* set_configuration */
-				usb_write(0, NULL, 0);
-				usb_set_address(s->wValueL & 0x7f);
-			}
-			else if (s->bRequest == 0x9) { /* set_address */
-				usb_write(0, NULL, 0);
-				if (__usb_ops.config)
-					__usb_ops.config(s_wValue);
-				goto token_done;
-			}
-		}
-		else if (pid == USB_TOKEN_IN) {
+		switch (pid) {
+		case USB_TOKEN_SETUP:
+			eps[ep].tx_data0 = DATA01_MASK;
+			break;
+		case USB_TOKEN_IN:
 			if (_usb_device_addr) {
 				regs8[REG_USB0_ADDR] = _usb_device_addr;
 				_usb_device_addr = 0;
 			}
-			if (_len)
-				usb_write(0, _tx, _len);
+			__usb_flush(ep);
+			break;
+		default:
+			break;
 		}
 
 		if (__usb_ops.token)
-			__usb_ops.token(pid, (uint8_t*)bd->address, bd->bc);
-token_done:
+			__usb_ops.token(pid, ep, (uint8_t*)bd->address, bd->bc);
 		regs8[REG_USB0_CTL] = USB_CTL_USBENSOFEN_MASK;
 		regs8[REG_USB0_ISTAT] = USB_ISTAT_TOKDNE_MASK;
 	}
@@ -238,13 +218,18 @@ token_done:
 	}
 
 	if (mask & USB_ISTAT_ERROR_MASK) {
-		printf("usb error %02x\n", regs8[REG_USB0_ERRSTAT]);
 		regs8[REG_USB0_ERRSTAT] = 0xff;
 		regs8[REG_USB0_ISTAT] |= USB_ISTAT_ERROR_MASK;
 	}
 
+	if (mask & USB_ISTAT_SOFTOK_MASK) {
+		if (__usb_ops.sof)
+			__usb_ops.sof();
+		regs8[REG_USB0_ISTAT] |= USB_ISTAT_SOFTOK_MASK;
+	}
+
 	if (!(stat & 0x8)) { /* !tx */
-		bd->bc = EP0_BUFSIZE;
+		bd->bc = eps[ep].rx_bufsize;
 		bd->flags = OWN_MASK;
 	}
 
@@ -254,6 +239,7 @@ int usb_enable()
 {
 	nvic_set_handler(NVIC_IRQ_USBOTG, __usb_irq_handler);
 	regs8[REG_USB0_INTEN] = USB_ISTAT_USBRST_MASK | USB_ISTAT_TOKDNE_MASK |
+		USB_ISTAT_SOFTOK_MASK |
 		USB_ISTAT_SLEEP_MASK | USB_ISTAT_ERROR_MASK;
 	regs8[REG_USB0_ERREN] = 0xff;
 	regs8[REG_USB0_USBCTRL] = 0;
@@ -275,26 +261,48 @@ int usb_disable()
 	return 0;
 }
 
-int usb_write(uint8_t ep, const uint8_t *buf, int len)
+static int __usb_flush(uint8_t ep)
 {
-	int idx = ep << 2 | 0x2 | odd;
+	struct endpoint_t *e = &eps[ep];
+	int idx = ep << 2 | 0x2 | e->tx_odd;
 	struct bdt_t *bd = &usb->bdt[idx];
-	int l = len < EP0_BUFSIZE ? len : EP0_BUFSIZE;
+	int l = e->tx_pend_len < e->tx_pktlen ? e->tx_pend_len : e->tx_pktlen;
+
+	if (!e->tx_valid)
+		return 0;
+
 	if (bd->flags & OWN_MASK)
 		return -EAGAIN;
-	bd->address = (int32_t)buf;
-	bd->bc = l;
-	if (len > EP0_BUFSIZE) {
-		_tx = (uint8_t*)(bd->address + EP0_BUFSIZE);
-		_len = len - EP0_BUFSIZE;
-	}
-	else {
-		_tx = NULL;
-		_len = 0;
-	}
-	bd->flags = OWN_MASK | data0;
 
-	data0 ^= DATA01_MASK;
-	odd ^= 1;
+	/* Send packet */
+	bd->address = (uint32_t)e->tx_pend;
+	bd->bc = l;
+	bd->flags = OWN_MASK | e->tx_data0;
+
+	/* Prepare for next flush, if any */
+	e->tx_odd ^= 1;
+	e->tx_pend_len -= l;
+	e->tx_data0 ^= DATA01_MASK;
+	if (e->tx_pend_len == 0)
+		e->tx_valid = 0;
+	else
+		e->tx_pend += l;
+
 	return l;
+}
+
+int usb_write(uint8_t ep, const uint8_t *buf, int len)
+{
+	struct endpoint_t *e;
+	if (ep > USB_EP_NUM)
+		return -EINVAL;
+	e = &eps[ep];
+
+	if (e->tx_valid)
+		return -EAGAIN;
+	e->tx_pend = buf;
+	e->tx_pend_len = len;
+	e->tx_valid = 1;
+	__usb_flush(ep);
+	return len;
 }
