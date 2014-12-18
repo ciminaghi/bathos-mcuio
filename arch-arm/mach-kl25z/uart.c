4ff36a79366f252244d97cf342b67b7d66723de2
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2014-12-18 11:29:35 +0100

    KL25Z: extend uart driver to handle all available ports: uart0, uart1, uart2
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch-arm/mach-kl25z/uart.c
+++ arch-arm/mach-kl25z/uart.c
@@ -19,47 +19,76 @@
 #include <bathos/io.h>
 #include <bathos/dev_ops.h>
 
-static uint8_t initialized;
-static struct bathos_dev __uart_dev;
+#define UIDX(x) (x - __kl25z_uarts)
+
+static struct bathos_dev __udev0, __udev1, __udev2;
 
 static void __uart_irq_handler(void);
 
-static int kl25z_uart_rx_enable(void *priv)
+struct kl25z_uart {
+	uint8_t tx_gpio;
+	uint8_t rx_gpio;
+	uint32_t irq;
+	int afnum;
+	int inited;
+};
+
+#define KL25ZUART(i, t, r, q, a) [i] = \
+	{.tx_gpio = t, .rx_gpio = r, .irq = q, .afnum = a}
+
+static struct kl25z_uart __kl25z_uarts[] = {
+	KL25ZUART(0, GPIO_NR(PORTE, 20), GPIO_NR(PORTE, 21), NVIC_IRQ_UART0, 4),
+	KL25ZUART(1, GPIO_NR(PORTE, 0), GPIO_NR(PORTE, 1), NVIC_IRQ_UART1, 3),
+	KL25ZUART(2, GPIO_NR(PORTE, 22), GPIO_NR(PORTE, 23), NVIC_IRQ_UART2, 4),
+};
+
+static struct bathos_dev *__uart_devs[] = {&__udev0, &__udev1, &__udev2};
+
+static int kl25z_uart_rx_enable(void *_priv)
 {
+	struct kl25z_uart *priv = (struct kl25z_uart*)_priv;
 	volatile uint8_t *regs8 = (void*)regs;
-	nvic_set_handler(NVIC_IRQ_UART2, __uart_irq_handler);
-	nvic_enable(NVIC_IRQ_UART2);
-	regs8[REG_UART_C2(2)] |= (UART_C2_RIE_MASK | UART_C2_RE_MASK);
+	gpio_dir_af(priv->rx_gpio, 0, 1, 4);
+	nvic_set_handler(priv->irq, __uart_irq_handler);
+	nvic_enable(priv->irq);
+	regs8[REG_UART_C2(UIDX(priv))] |= (UART_C2_RIE_MASK | UART_C2_RE_MASK);
 	return 0;
 }
 
-static int kl25z_uart_tx_enable(void *priv)
+static int kl25z_uart_tx_enable(void *_priv)
 {
+	struct kl25z_uart *priv = (struct kl25z_uart*)_priv;
 	volatile uint8_t *regs8 = (void*)regs;
-	regs8[REG_UART_C2(2)] |= UART_C2_TE_MASK;
+	gpio_dir_af(priv->tx_gpio, 1, 1, 4);
+	regs8[REG_UART_C2(UIDX(priv))] |= UART_C2_TE_MASK;
 	return 0;
 }
 
-static int kl25z_uart_rx_disable(void *priv)
+static int kl25z_uart_rx_disable(void *_priv)
 {
+	struct kl25z_uart *priv = (struct kl25z_uart*)_priv;
 	volatile uint8_t *regs8 = (void*)regs;
-	regs8[REG_UART_C2(2)] &= ~((UART_C2_RIE_MASK | UART_C2_RE_MASK));
-	nvic_disable(NVIC_IRQ_UART2);
+	gpio_dir_af(priv->rx_gpio, 0, 1, 0);
+	regs8[REG_UART_C2(UIDX(priv))] &= ~((UART_C2_RIE_MASK | UART_C2_RE_MASK));
+	nvic_disable(priv->irq);
 	return 0;
 }
 
-static int kl25z_uart_tx_disable(void *priv)
+static int kl25z_uart_tx_disable(void *_priv)
 {
+	struct kl25z_uart *priv = (struct kl25z_uart*)_priv;
 	volatile uint8_t *regs8 = (void*)regs;
-	regs8[REG_UART_C2(2)] &= ~UART_C2_TE_MASK;
+	gpio_dir_af(priv->tx_gpio, 0, 1, 0);
+	regs8[REG_UART_C2(UIDX(priv))] &= ~UART_C2_TE_MASK;
 	return 0;
 }
 
-static int kl25z_uart_putc(void *priv, const char c)
+static int kl25z_uart_putc(void *_priv, const char c)
 {
+	struct kl25z_uart *priv = (struct kl25z_uart*)_priv;
 	volatile uint8_t *regs8 = (void*)regs;
-	while(!(regs8[REG_UART_S1(2)] & UART_S1_TDRE_MASK));
-	regs8[REG_UART_D(2)] = c;
+	while(!(regs8[REG_UART_S1(UIDX(priv))] & UART_S1_TDRE_MASK));
+	regs8[REG_UART_D(UIDX(priv))] = c;
 	return 1;
 }
 
@@ -76,61 +105,65 @@ static void __uart_irq_handler(void)
 	volatile uint8_t *regs8 = (void*)regs;
 	char c;
 	uint8_t s1;
-	/* read of S1 register is needed for intr flag clearing */
+	int u;
 
-	s1 = regs8[REG_UART_S1(2)];
-	if ((s1 & UART_S1_RDRF_MASK) == 0)
-		return;
-
-	c = regs8[REG_UART_D(2)];
-	(void)bathos_dev_push_chars(&__uart_dev, &c, 1);
+	for (u = 0; u < ARRAY_SIZE(__kl25z_uarts); u++) {
+		if (!__kl25z_uarts[u].inited)
+			continue;
+		s1 = regs8[REG_UART_S1(u)];
+		if ((s1 & UART_S1_RDRF_MASK) == 0)
+			continue;
+		c = regs8[REG_UART_D(u)];
+		(void)bathos_dev_push_chars(__uart_devs[u], &c, 1);
+	}
 }
 
-static int kl25z_uart_set_baudrate(uint32_t baud)
+static int kl25z_uart_set_baudrate(uint8_t u, uint32_t baud)
 {
 	/* set BDH and BDL */
 	uint32_t bd = BUS_FREQ / 16 / baud;
 	volatile uint8_t *regs8 = (void*)regs;
-	regs8[REG_UART_BDH(2)] &= ~(0x1f);
-	regs8[REG_UART_BDH(2)] |= (bd >> 8) & 0x1f;
-	regs8[REG_UART_BDL(2)] = bd & 0xff;
+	regs8[REG_UART_BDH(u)] &= ~(0x1f);
+	regs8[REG_UART_BDH(u)] |= (bd >> 8) & 0x1f;
+	regs8[REG_UART_BDL(u)] = bd & 0xff;
 	return 0;
 }
 
-static int kl25z_uart_init(void)
+static int kl25z_uart_init(int u)
 {
 	void *udata;
-	if (initialized)
+	if (__kl25z_uarts[u].inited)
 		return 0;
-	/* Init UART2, target baud rate = 250000 */
-	regs[REG_SCGC4] |= SIM_SCGC4_UART2_MASK;
-	kl25z_uart_set_baudrate(250000);
-	udata = bathos_dev_init(&kl25z_dev_ops, NULL);
+
+	/* Init UART #u, target baud rate = 250000 */
+	switch (u) {
+		case 0: regs[REG_SCGC4] |= SIM_SCGC4_UART0_MASK; break;
+		case 1: regs[REG_SCGC4] |= SIM_SCGC4_UART1_MASK; break;
+		case 2: regs[REG_SCGC4] |= SIM_SCGC4_UART2_MASK; break;
+	}
+
+	kl25z_uart_set_baudrate(u, 250000);
+	udata = bathos_dev_init(&kl25z_dev_ops, &__kl25z_uarts[u]);
 	if (!udata)
 		return -ENODEV;
-	__uart_dev.priv = udata;
-	initialized = 1;
-	/* FIXME set af on tx/rx enable/disable funcs */
-	gpio_dir_af(GPIO_NR(PORTE, 22), 1, 1, 4);
-	gpio_dir_af(GPIO_NR(PORTE, 23), 0, 1, 4);
+	__uart_devs[u]->priv = udata;
+	__kl25z_uarts[u].inited = 1;
 	return 0;
 }
 
 #if defined CONFIG_CONSOLE_UART && CONFIG_EARLY_CONSOLE
 int console_early_init(void)
 {
-	return kl25z_uart_init();
+	return kl25z_uart_init(2);
 }
-#else
-rom_initcall(kl25z_uart_init);
 #endif
 
 static int kl25z_uart_open(struct bathos_pipe *pipe)
 {
 	int stat = 0;
 
-	if (!initialized)
-		stat = kl25z_uart_init();
+	/* Take uart number from its name uartX */
+	stat = kl25z_uart_init(pipe->dev->name[4] - '0');
 	if (stat)
 		return stat;
 	return bathos_dev_open(pipe);
@@ -144,9 +177,20 @@ const struct bathos_dev_ops PROGMEM uart_dev_ops = {
 	.ioctl = bathos_dev_ioctl,
 };
 
-static struct bathos_dev __uart_dev
-	__attribute__((section(".bathos_devices"),
-					    aligned(2))) = {
+static struct bathos_dev __udev0
+	__attribute__((section(".bathos_devices"), aligned(4))) = {
+	.name = "uart0",
+	.ops = &uart_dev_ops,
+};
+
+static struct bathos_dev __udev1
+	__attribute__((section(".bathos_devices"), aligned(4))) = {
+	.name = "uart1",
+	.ops = &uart_dev_ops,
+};
+
+static struct bathos_dev __udev2
+	__attribute__((section(".bathos_devices"), aligned(4))) = {
 	.name = "uart2",
 	.ops = &uart_dev_ops,
 };
