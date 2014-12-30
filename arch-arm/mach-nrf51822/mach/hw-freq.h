bae6af170d857012378228305580baf65246b0e3
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-12-30 17:03:11 +0100

    ARM, add preliminary support for nordic nrf51822

---
+++ arch-arm/mach-nrf51822/mach/hw-freq.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
+ * Originally extracted from hw.h
+ */
+
+#ifndef __NRF51822_HW_FREQ__
+#define __NRF51822_HW_FREQ__
+
+#define HFCLK			(16 * 1000 * 1000)
+#define LFCLK			(32768)
+#define CPU_FREQ		HFCLK
+#define HZ			128
+#define BUS_FREQ		HFCLK
+
+#endif /* __NRF51822_HW_FREQ__ */
---
+++ arch-arm/mach-nrf51822/mach/hw.h
@@ -0,0 +1,10 @@
+/*
+ * Hardware registers we use, defined for use in the regs[] abstraction
+ * Alessandro Rubini, 2009-2012 GNU GPL2 or later
+ * Modified for KL25Z Davide Ciminaghi <ciminaghi@gnudd.com> 2014
+ */
+#ifndef __NRF51822_HW_H__
+#define __NRF51822_HW_H__
+
+
+#endif /* __NRF51822_HW_H__ */
