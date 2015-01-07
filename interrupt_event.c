0fb83cf598e776f4e35e1db112da388d17319a36
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-07 22:20:36 +0100

    add  interrupt events tables and default handlers
    interrupt_event.c is built once for every interrupt event. Resulting objs
    are then linked in proper order so that an array of struct event is setup.
    Dummy default (weak) handlers and private data are also defined for all
    interrupt events.

---
+++ interrupt_event.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Davide Ciminaghi <ciminaghi@gnudd.com>
+ */
+
+#include <bathos/event.h>
+
+#ifndef INTNO
+#error "Please compile this file with -DINTNO=interrupt_number"
+#endif
+
+#define ISR_NAME bathos_int_handler_name(INTNO)
+#define ISR_PRIV bathos_int_handler_priv(INTNO)
+#define EVT_NAME xcat(int_evt_,INTNO)
+
+/* Dummy interrupt event handler and relevant data, can be overridden */
+void  __attribute__((weak)) ISR_NAME(struct event_handler_data *d)
+{
+}
+
+struct { } ISR_PRIV __attribute__((weak));
+
+static const struct event_handler_ops PROGMEM
+event_handler_ops_struct(ISR_NAME) = {
+	.init = NULL,
+	.handle = ISR_NAME,
+	.exit = NULL,
+};
+
+static struct event_handler_data PROGMEM event_handler_struct(ISR_NAME) = {
+	.ops = &event_handler_ops_struct(ISR_NAME),
+	.priv = &ISR_PRIV,
+};
+
+/*
+ * Interrupts can have just one handler
+ */
+const struct event __attribute__((section(".interrupt_events"))) EVT_NAME = {
+	.handlers_start = &event_handler_struct(ISR_NAME),
+	.handlers_end = &(&event_handler_struct(ISR_NAME))[1],
+};
+
+
