bc56653f14e047ce180ec8f6f15dd1de49e3c24e
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-09 12:17:23 +0100

    ARM, KL25Z, jiffies: convert to new irq api

--- arch-arm/mach-kl25z/jiffies.c
+++ arch-arm/mach-kl25z/jiffies.c
@@ -8,16 +8,11 @@
 #include <bathos/jiffies.h>
 #include <bathos/event.h>
 #include <bathos/sys_timer.h>
+#include <bathos/irq.h>
 #include <arch/hw.h>
 #include <mach/hw.h>
 
-#ifdef CONFIG_MBED_DRIVERS
-#include <cmsis_nvic.h>
-#else
-#include <cpu-cortex-m0/nvic.h>
-#endif
-
-static void pit_irq_handler(void)
+void bathos_int_handler_name(NVIC_IRQ_PIT)(void)
 {
 	jiffies++;
 	regs[REG_PIT_TFLG(0)] = PIT_TFLG_TIF_MASK;
@@ -30,15 +25,9 @@ void jiffies_init(void)
 	regs[REG_SCGC6] |= SIM_SCGC6_PIT_MASK;
 	regs[REG_PIT_MCR] = 0;
 
-#ifdef CONFIG_MBED_DRIVERS
-	NVIC_SetVector(PIT_IRQn, (uint32_t)pit_irq_handler);
-	NVIC_EnableIRQ(PIT_IRQn);
-#else
-	nvic_set_handler(NVIC_IRQ_PIT, pit_irq_handler);
-	nvic_enable(NVIC_IRQ_PIT);
-#endif
-
 	/* Use channel 0 to generate irqs for jiffies update */
 	regs[REG_PIT_LDVAL(0)] = BUS_FREQ / HZ - 1;
 	regs[REG_PIT_TCTRL(0)] = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
+
+	bathos_enable_irq(NVIC_IRQ_PIT);
 }
