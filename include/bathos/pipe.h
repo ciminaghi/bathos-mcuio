fdcd3c049298c785c8ee7cf63d57fddbdae2065f
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-09 11:42:10 +0100

    dev ops: get bathos_dev_close to return int
    The close method of a device must return int (pipe_close checks the return code)

--- include/bathos/pipe.h
+++ include/bathos/pipe.h
@@ -27,7 +27,7 @@ struct bathos_dev_ops {
 	int (*open)(struct bathos_pipe *);
 	int (*read)(struct bathos_pipe *, char *buf, int len);
 	int (*write)(struct bathos_pipe *, const char *buf, int len);
-	void (*close)(struct bathos_pipe *);
+	int (*close)(struct bathos_pipe *);
 	int (*ioctl)(struct bathos_pipe *, struct bathos_ioctl_data *);
 };
 
