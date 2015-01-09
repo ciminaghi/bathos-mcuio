1a6bd2d631eab90fcb42412498221d4007a7a761
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-09 12:24:57 +0100

    ARM, KL25Z, init: nvic_init_ram() is not necessary anymore

--- arch-arm/mach-kl25z/init.c
+++ arch-arm/mach-kl25z/init.c
@@ -3,10 +3,8 @@
 #include <bathos/gpio.h>
 #include <bathos/init.h>
 #include <bathos/event.h>
-#include <cpu-cortex-m0/nvic.h>
 
 unsigned long jiffies;
-volatile uint32_t *vectors = rom_vectors;
 
 #ifdef CONFIG_CONSOLE_DEBUG_BITBANG
 static void console_gpio_init(void)
@@ -23,7 +21,6 @@ static void console_gpio_init(void)
 static int mach_ll_init(void)
 {
 	clocks_init();
-	nvic_init_ram();
 	jiffies_init();
 	console_gpio_init();
 	events_init();
