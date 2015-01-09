ee8191854180fa949d623f0516616f1d6526e22e
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-09 16:21:41 +0100

    nrf51822, hw.h: add irq numbers

--- arch-arm/mach-nrf51822/mach/hw.h
+++ arch-arm/mach-nrf51822/mach/hw.h
@@ -6,5 +6,30 @@
 #ifndef __NRF51822_HW_H__
 #define __NRF51822_HW_H__
 
+#define POWER_CLOCK_IRQ 0
+#define RADIO_IRQ	1
+#define UART0_IRQ	2
+#define SPI0_TWI0_IRQ	3
+#define SPI1_TWI1_IRQ	4
+#define GPIOTE_IRQ	6
+#define ADC_IRQ		7
+#define TIMER0_IRQ	8
+#define TIMER1_IRQ	9
+#define TIMER2_IRQ	10
+#define RTC0_IRQ	11
+#define TEMP_IRQ	12
+#define RNG_IRQ		13
+#define ECB_IRQ		14
+#define CCM_AAR_IRQ	15
+#define WDT_IRQ		16
+#define RTC1_IRQ	17
+#define QDEC_IRQ	18
+#define LPCOMP_COMP_IRQ 19
+#define SWI0_IRQ	20
+#define SWI1_IRQ	21
+#define SWI2_IRQ	22
+#define SWI3_IRQ	23
+#define SWI4_IRQ	24
+#define SWI5_IRQ	25
 
 #endif /* __NRF51822_HW_H__ */
