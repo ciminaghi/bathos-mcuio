e0260db38a237c401a397688fcbe8ec8de209607
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-13 06:12:46 +0100

    ARM, add common inline helper for timer tick irq handler

---
+++ arch-arm/arch/systick.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Davide Ciminaghi <ciminaghi@gnudd.com>
+ */
+#include <bathos/jiffies.h>
+#include <bathos/event.h>
+
+static inline void bathos_arm_sys_tick_timer_handler(void)
+{
+	jiffies++;
+	trigger_event(&event_name(hw_timer_tick), NULL);
+}
