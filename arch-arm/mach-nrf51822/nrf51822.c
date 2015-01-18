f6e5f5a8b9b6bf9fa93744ab2b202d2ad974bf30
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-18 06:48:02 +0100

    ARM, NRF51822: instantiate NRF51822 uart

--- arch-arm/mach-nrf51822/nrf51822.c
+++ arch-arm/mach-nrf51822/nrf51822.c
@@ -8,6 +8,8 @@
 #include <bathos/init.h>
 #include <bathos/event.h>
 #include <bathos/nrf51-rtc.h>
+#include <bathos/nrf51-uart.h>
+#include <bathos/dev_ops.h>
 #include <mach/hw.h>
 
 
@@ -26,4 +28,30 @@ core_initcall(rtc_init);
 void bathos_ll_int_handler_name(RTC0_IRQ)(struct event_handler_data *data)
 {
 	nrf51_irq_handler(&rtc0_plat);
+}
+
+static const struct nrf51_uart_platform_data uart0_plat = {
+	.tx_pin = 10,
+	.rx_pin = 11,
+	.irq = UART0_IRQ,
+	.base = UART0_BASE,
+};
+
+static struct bathos_dev __udev0;
+
+static struct nrf51_uart_priv uart0_priv = {
+	.dev = &__udev0,
+	.platform_data = &uart0_plat,
+};
+
+static struct bathos_dev __udev0
+	__attribute__((section(".bathos_devices"), aligned(4))) = {
+	.name = "uart0",
+	.ops = &nrf51_uart_dev_ops,
+	.priv = &uart0_priv,
+};
+
+void bathos_ll_int_handler_name(IRQ_UART0)(struct event_handler_data *data)
+{
+	nrf51_uart_irq_handler(&uart0_priv);
 }
