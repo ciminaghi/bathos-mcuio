5d18ff20a9106237851803233350fd2141f9d8a3
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-01-20 21:40:57 +0100

    tasks/mcuio_gpio_func.c: no special case for kl25's gpio labels/capabilities.
    Automatic generation of gpio related tables (labels and capabilities), we
    don't need any special treatment for the kl25z (and all arm machines).

--- tasks/mcuio_gpio_func.c
+++ tasks/mcuio_gpio_func.c
@@ -20,7 +20,6 @@ extern struct mcuio_function gpio;
 declare_extern_event(gpio_evt);
 declare_extern_event(mcuio_irq);
 
-#ifdef CONFIG_ARCH_ATMEGA
 extern const unsigned int gpio_labels_size;
 extern const unsigned int gpio_caps_size;
 extern const unsigned int gpio_evts_caps_size;
@@ -30,11 +29,6 @@ static const unsigned int PROGMEM __gpio_caps_size =
 	(const unsigned int)&gpio_caps_size;
 static const unsigned int PROGMEM __gpio_evts_caps_size =
 	(const unsigned int)&gpio_evts_caps_size;
-#elif defined(CONFIG_MACH_KL25Z)
-const unsigned int __gpio_labels_size = 4 * MCUIO_NGPIO;
-const unsigned int __gpio_caps_size = MCUIO_NGPIO;
-const unsigned int __gpio_evts_caps_size = MCUIO_NGPIO;
-#endif
 
 extern const char PROGMEM gpio_labels_start[];
 extern const uint32_t PROGMEM gpio_caps_start[];
