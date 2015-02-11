3db1328b91e149535a6bed333cb89c739312cbcd
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-02-11 15:24:20 +0100

    ARM, init.c: remove now useless default _romboot_start()

--- arch-arm/init.c
+++ arch-arm/init.c
@@ -18,11 +18,6 @@ volatile unsigned long __attribute__((weak)) jiffies;
 volatile unsigned long __attribute__((weak)) _sdata;
 volatile unsigned long __attribute__((weak)) _erom;
 
-/* Dummy start function (replaced by machine's romboot_start in boot.S */
-void __attribute__((weak)) _romboot_start(void)
-{
-}
-
 int stdio_init(void)
 {
 	return 0;
