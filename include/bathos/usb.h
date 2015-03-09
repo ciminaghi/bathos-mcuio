ede3042b5f06f05e9ec49867cf4d9a508ede25ea
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-03-09 22:11:24 +0100

    Add usb header file (generic definitions)

---
+++ include/bathos/usb.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Aurelio Colosimo <aurelio@aureliocolosimo.it>
+ *
+ * Attempt to make a cleaner distinction between high and low level drivers
+ * and general code reorganization by Davide Ciminaghi <ciminaghi@gnudd.com>
+ */
+#ifndef __BATHOS_USB_H__
+#define __BATHOS_USB_H__
+
+#include <stdint.h>
+
+/*
+ * Generic usb related definitions
+ */
+
+/* These defines are for the 'type' argument of token_cb */
+#define USB_TOKEN_IN	0x9
+#define USB_TOKEN_OUT	0x1
+#define USB_TOKEN_SETUP	0xd
+
+/* standard control endpoint request types */
+#define USB_GET_STATUS		0
+#define USB_CLEAR_FEATURE	1
+#define USB_SET_FEATURE		3
+#define USB_SET_ADDRESS		5
+#define USB_GET_DESCRIPTOR	6
+#define USB_GET_CONFIGURATION	8
+#define USB_SET_CONFIGURATION	9
+#define USB_GET_INTERFACE	10
+#define USB_SET_INTERFACE	11
+#define USB_SYNCH_FRAME		12
+
+/*
+ * Setup data
+ */
+struct usb_setup_data {
+	uint8_t bmRequestType;
+	uint8_t bRequest;
+	uint8_t wValueL;
+	uint8_t wValueH;
+	uint8_t wIndexL;
+	uint8_t wIndexH;
+	uint8_t wLengthL;
+	uint8_t wLengthH;
+};
+
+/*
+ * A setup request (setup token + data)
+ */
+struct usb_setup_request {
+	uint8_t pid;
+	struct usb_setup_data *data;
+};
+
+#endif /* __BATHOS_USB_H__ */
