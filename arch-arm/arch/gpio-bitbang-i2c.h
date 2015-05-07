b5492b654a09bd30b55b4d21b6d9d544fdaeb149
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-05-07 18:26:22 +0200

    ARM, provide an empty gpio-bitbang-i2c.h
    This is empty for now, since the kl25z has no need to define specific
    setsda, setscl functions.

---
+++ arch-arm/arch/gpio-bitbang-i2c.h
@@ -0,0 +1,11 @@
+#ifndef __GPIO_BITBANG_H__
+#define __GPIO_BITBANG_H__
+
+/* This is empty for the moment */
+
+/*
+ * Include machine specific gpio-bitbang-i2c.h here
+ * (<mach/gpio-bitbang-i2c.h>)
+ */
+
+#endif __GPIO_BITBANG_H__
