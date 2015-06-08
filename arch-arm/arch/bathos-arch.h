3dabad559b28bcb846aa0553a14547a176dba41c
Davide Ciminaghi <ciminaghi@gnudd.com>
2015-06-08 21:53:02 +0200

    ARM: add basic support for ARM926EJ-S

--- arch-arm/arch/bathos-arch.h
+++ arch-arm/arch/bathos-arch.h
@@ -2,5 +2,11 @@
 
 #define PROGMEM
 
+#if defined CONFIG_CPU_CORTEX_M0 || defined CONFIG_CPU_CORTEX_M0_PLUS || \
+    defined CONFIG_CPU_CORTEX_M3
 #include <arch/bathos-arch-cortex-m.h>
+#endif
 
+#if defined CONFIG_CPU_ARM926EJS
+#include <arch/bathos-arch-arm926ej-s.h>
+#endif
