1483ce9e13868c6284ad9abbb405fa64b5066c87
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-03-16 05:56:14 +0100

    ARM, reorganize arch headers
    scb-cortex-m0 should actually be scb-cortex-m, since many registers are
    common to all cortex processors.
    The nvic is also very similar for all cortexes.

--- arch-arm/arch/nvic.h
+++ arch-arm/arch/nvic.h
@@ -5,9 +5,12 @@
  */
 
 #include <generated/autoconf.h>
+#include <arch/scb-cortex-m.h>
 
-#if defined CONFIG_CPU_CORTEX_M0 || defined CONFIG_CPU_CORTEX_M0_PLUS
-#include <arch/nvic-cortex-m0.h>
-#include <arch/scb-cortex-m0.h>
-#endif
+#define NVIC_CORE_NUM		16
 
+#define NVIC_IRQ_NMI 		( 2 - NVIC_CORE_NUM)
+#define NVIC_IRQ_HARD_FAULT 	( 3 - NVIC_CORE_NUM)
+#define NVIC_IRQ_SVCALL 	(11 - NVIC_CORE_NUM),
+#define NVIC_IRQ_PENDSV		(14 - NVIC_CORE_NUM),
+#define NVIC_IRQ_SYSTICK	(15 - NVIC_CORE_NUM),
