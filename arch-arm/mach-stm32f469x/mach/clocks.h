ee2baed37920681b134813aa48ce357cb142b4af
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-03-19 07:17:00 +0100

    ARM, STM32F469X: add clocks header file

---
+++ arch-arm/mach-stm32f469x/mach/clocks.h
@@ -0,0 +1,12 @@
+/*
+ * clocks header for stm32f469x machine
+ */
+#ifndef __STM32F469X_CLOCKS_H__
+#define __STM32F469X_CLOCKS_H__
+
+#include <family/clocks.h>
+
+extern int clocks_init(void);
+
+#endif /* __STM32F469X_CLOCKS_H__ */
+
