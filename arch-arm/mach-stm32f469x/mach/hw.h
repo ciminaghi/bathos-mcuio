0e3f11262b00ffeee184977c0b28350805044e5f
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-03-19 06:26:55 +0100

    ARM, STM32F469x, add hw.h

---
+++ arch-arm/mach-stm32f469x/mach/hw.h
@@ -0,0 +1,14 @@
+/*
+ * Header file for STM32F469/479x
+ *
+ * Copyright Dog Hunter SA 2016
+ * GPLv2 or later
+ * Author: Davide Ciminaghi (comes from kl25z hw.h)
+ */
+#ifndef __STM32F469X_H__
+#define __STM32F469X_H__
+
+#define RCC_BASE 0x40023800
+
+
+#endif /* __STM32F469X_H__ */
