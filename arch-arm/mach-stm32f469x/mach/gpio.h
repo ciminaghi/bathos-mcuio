28c4580d8434d07c95f579333c81e69d7db77236
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-03-25 12:51:43 +0100

    ARM, STM32F469X: add gpio header file.
    We just refer to the family's gpio header.

---
+++ arch-arm/mach-stm32f469x/mach/gpio.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) dog hunter AG - Zug - CH
+ * General Public License version 2 (GPLv2)
+ * Author: Davide Ciminaghi <ciminaghi@gnudd.com>
+ */
+#ifndef __GPIO_STM32F469X_H__
+#define __GPIO_STM32F469X_H__
+
+#include <family/gpio.h>
+
+
+#endif /* __GPIO_STM32F469X_H__ */
