2125065118f94c1d05559f7ede75f477f6b7b0ef
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-03-25 14:55:02 +0100

    ARM, STM32F469X: setup clocks on mach low-level init

--- arch-arm/mach-stm32f469x/init.c
+++ arch-arm/mach-stm32f469x/init.c
@@ -1,10 +1,11 @@
 #include <bathos/init.h>
 #include <stdint.h>
 #include <arch/hw.h>
+#include <mach/clocks.h>
 
 
 int mach_ll_init(void)
 {
-	return 0;
+	return stm32f4x_sysclock_init();
 }
 rom_initcall(mach_ll_init);
