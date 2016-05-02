a36cc4f45af72402b9c933d0feb8543f7205aa06
Davide Ciminaghi <ciminaghi@gnudd.com>
2016-05-03 00:05:42 +0200

    XTENSA: add interrupt_disable/restore implementation

--- arch-xtensa/arch/bathos-arch.h
+++ arch-xtensa/arch/bathos-arch.h
@@ -3,8 +3,16 @@
 
 #define PROGMEM
 
-#define interrupt_disable(a) a=0
-#define interrupt_restore(a) a*=2
+/*
+ * From https://github.com/esp8266/Arduino/pull/649/files and xtensa
+ * instruction set manual
+ */
+
+#define interrupt_disable(a)						\
+        __asm__ __volatile__("rsil %0, 15" : "=a" (a))
+
+#define interrupt_restore(a)						\
+	__asm__ __volatile__("wsr %0,ps; isync" :: "a" (a) : "memory")
 
 #endif /* __BATHOS_ARCH_H__ */
 
